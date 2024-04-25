3796b334930832a345084e25723dfca8d60a0acd
Jashan Bansal <jashanbansal7861@gmail.com>
2024-04-25 11:36:35 +0530

    complete the project

---
+++ src/components/Order.js
@@ -0,0 +1,82 @@
+import React, {  useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import { motion } from 'framer-motion';
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: '100vw'
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: 'spring', 
+      mass: 0.4,
+      damping: 8,
+      when: "beforeChildren",
+      staggerChildren: 0.4
+    }
+  },
+  exit: {
+    x: '-100vw',
+    transition: { ease: 'easeInOut' }
+  }
+}
+
+const childVariants = {
+  hidden: {
+    opacity: 0
+  }, 
+  visible: {
+    opacity: 1
+  }
+}
+
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      duration: 0.3,
+      yoyo: Infinity
+    } 
+  }
+}
+
+
+const Order = ({ pizza, setShowModal }) => {
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowModal(true)
+    }, 5000)
+  }, [setShowModal])
+
+  return (
+    <motion.div className="container order"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
+      <h2>Thank you for your order :)</h2>
+      <motion.p variants={childVariants}>You ordered a {pizza.base} pizza with:</motion.p>
+      <motion.div variants={childVariants}>
+        {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
+      </motion.div>
+      <Link to="/">
+            <motion.button
+              variants={buttonVariants}
+              whileHover="hover"
+            >
+              Home
+            </motion.button>
+          </Link>
+    </motion.div>
+  )
+}
+
+export default Order;
